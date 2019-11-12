d6450059b5ff64bc214fa7af4f7be53a5ab46558
Augusto F. de Abreu <augustoabreu.202@gmail.com>
2019-11-12 19:14:22 -0400

    fazendo alterações em produtos

--- src/router.js
+++ src/router.js
@@ -2,17 +2,13 @@ import Vue from 'vue';
 import Router from 'vue-router';
 import Setor from './views/Setor.vue';
 import Contato from './views/Contato.vue';
-<<<<<<< HEAD
 import Produto from './views/Produto.vue'
-=======
 import Mensagem from './views/Mensagem.vue';
 import Carrinho from "./views/Carrinho.vue"
->>>>>>> d18375ae5599b0b485521f38de1b1698a4fb8435
 Vue.use(Router);
 
 
 export default new Router({
-<<<<<<< HEAD
     mode: 'history',
     base: process.env.BASE_URL,
     routes: [
@@ -31,19 +27,6 @@ export default new Router({
       name: 'Produto',
       component: Produto
     },
-=======
-  mode: 'history',
-  base: process.env.BASE_URL,
-  routes: [{
-      path: '/setores',
-      name: 'Setores',
-      component: Setor
-    },
-    {
-      path: '/contato',
-      name: 'Contato',
-      component: Contato
-    },
     {
       path: '/mensagem',
       name: 'Mensagem',
@@ -55,6 +38,5 @@ export default new Router({
       component: Carrinho
     },
 
->>>>>>> d18375ae5599b0b485521f38de1b1698a4fb8435
   ]
 })
