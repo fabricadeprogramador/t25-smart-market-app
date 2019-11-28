b522c3a30dfa1dcb380eea4d65badf2b36c99034
f3l1p3v4 <fvaldez018@gmail.com>
2019-11-28 18:48:44 -0500

    Criado metodo de buscar produtos

--- src/views/Produto.vue
+++ src/views/Produto.vue
@@ -1,26 +1,13 @@
 <template>
-<v-container-fluid> 
-  <v-row >
-    <v-col cols="4" v-for="produto in produtos" :key="produto._id" >
-      <v-card class="ml-2 mr-1">
-        <v-card-title>
-            <v-dialog
-              v-model="dialog"
-              fullscreen
-              persistent
-              transition="dialog-bottom-transition"
-            >
+  <v-container-fluid>
+    <v-row>
+      <v-col cols="4" v-for="produto in produtos" :key="produto._id">
+        <v-card class="ml-2 mr-1">
+          <v-card-title>
+            <v-dialog v-model="dialog" fullscreen persistent transition="dialog-bottom-transition">
               <template v-slot:activator="{ on }">
-                <v-btn
-                  text
-                  icon
-                  color="black"
-                  v-on="on"
-                  @click="abrirDetalhes(produto)"
-                >
-                  <v-icon>
-                    mdi-chevron-down
-                  </v-icon>
+                <v-btn text icon color="black" v-on="on" @click="abrirDetalhes(produto)">
+                  <v-icon>mdi-chevron-down</v-icon>
                 </v-btn>
               </template>
 
@@ -32,7 +19,7 @@
                   <v-spacer></v-spacer>
                 </v-toolbar>
 
-                <div class="detalhes" >
+                <div class="detalhes">
                   <v-img
                     style="margin:auto ; border: 1px solid black"
                     max-width="400px"
@@ -40,50 +27,43 @@
                     class="d-flex child-flex"
                     gradient="to bottom, rgba(0,0,0,0), rgba(0,0,0,0)"
                     height="200px"
-                  >
-                  </v-img>
+                  ></v-img>
                   <p class="title ml-6">
                     quantidade disponivel:
                     {{ produtoSelecionado.quantidade }} unidades
                   </p>
-                  <v-text-field
-                    class="d-sm-inline-flex ml-6 "
-                    v-model="qtdUnidade"
-                  ></v-text-field>
+                  <v-text-field class="d-sm-inline-flex ml-6" v-model="qtdUnidade"></v-text-field>
                   <p
                     class="text-right text-uppercase mr-6"
                     style="font-size:35pt ; color:red"
-                  >
-                    R${{ produtoSelecionado.valor }},00
-                  </p>
-                  <v-btn color="amber"  class="justify-center  " @click="adicionarCarrinho"
-                    >Adicionar ao Carrinho</v-btn
-                  >
+                  >R${{ produtoSelecionado.valor }},00</p>
+                  <v-btn
+                    color="amber"
+                    class="justify-center"
+                    @click="adicionarCarrinho"
+                  >Adicionar ao Carrinho</v-btn>
                 </div>
               </v-card>
             </v-dialog>
-        </v-card-title>
+          </v-card-title>
 
-        <v-card-subtitle class="display-1 black--text">
-          <v-img
-            :src="produto.imagem"
-            class="pa-auto ma-auto"
-            max-width="300px"
-            max-height="300px"
-            style="whidth:200px ; height:200px"
-          >
-          </v-img>
+          <v-card-subtitle class="display-1 black--text">
+            <v-img
+              :src="produto.imagem"
+              class="pa-auto ma-auto"
+              max-width="300px"
+              max-height="300px"
+              style="whidth:200px ; height:200px"
+            ></v-img>
 
-          <p class="font-weight-bold" v-text="produto.nome"></p>
+            <p class="font-weight-bold" v-text="produto.nome"></p>
 
-          <p class="title font-weight-bold">
-            R$ {{ produto.valor.toFixed(2) }}
-          </p>
-        </v-card-subtitle>
-      </v-card>
-    </v-col>
-  </v-row>
-</v-container-fluid>
+            <p class="title font-weight-bold">R$ {{ produto.valor}}</p>
+          </v-card-subtitle>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container-fluid>
 </template>
 
 <script>
@@ -98,9 +78,7 @@ export default {
 
   methods: {
     buscarProdutos() {
-      HttpRequestUtil.buscarProdutos().then(produtos => {
-        this.produtos = produtos;
-      });
+      this.produtos = JSON.parse(localStorage.getItem("produtosPorSetor"));
     },
 
     abrirDetalhes(produto) {
@@ -150,11 +128,10 @@ export default {
   width: 100%;
   margin-top: 1%;
 }
-.imagem-detalhe{
-  
+.imagem-detalhe {
   padding: 20px;
-  
-  border: 1px solid black
+
+  border: 1px solid black;
 }
 .dados {
   text-align: center;
