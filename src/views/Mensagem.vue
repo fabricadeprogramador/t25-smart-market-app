95831acfeb70a8587b0bbbea66be7da79b1d7bc2
ABabayaga <alef_rdl@hotmail.com>
2019-11-28 20:10:59 -0400

    Ajustando Mensagem

--- src/views/Mensagem.vue
+++ src/views/Mensagem.vue
@@ -1,21 +1,48 @@
-<template>
-  <div class="mx-2 my-4">
-    <v-alert type="info"> <input v-model="resposta"></v-alert>
-  </div>
+
+<template v-for="(item, index) in items" >
+
+
+    <v-list rounded>
+<v-alert
+      dense
+      type="info"
+    >
+
+      
+        <v-list-item
+          v-for="(item, i) in items"
+          :key="i"
+        >
+          <v-list-item-content>
+            <v-list-item-title v-text="item.resposta"></v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+   </v-alert>
+   </v-list>
+    
+  
+  
 </template>
 
 <script>
 import HttpRequestUtil from "@/util/HttpRequestUtil";
 export default {
   data: () => ({
-    resposta: '',
+    item: 1,
+      items: [
+        { text: 'Real-Time', icon: 'mdi-clock' },
+        { text: 'Audience', icon: 'mdi-account' },
+        { text: 'Conversions', icon: 'mdi-flag' },
+      ],
   }),
   methods: {},
   mounted() {
     if (localStorage.getItem("clienteLogado") != null) {
       let cliente = JSON.parse(localStorage.getItem("clienteLogado"));
+
       HttpRequestUtil.buscarPorCliente(cliente).then(response => {
-        this.resposta = response;
+        // alert(JSON.stringify(response));
+        this.items = response;
       });
     }
   }
